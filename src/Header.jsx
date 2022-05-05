ec219f763b32b09c398d703036c9bf825d457527
AM5090 <armen.melkonyan.93@inbox.ru>
2022-05-05 11:15:19 +0300

    Проект на CSR

--- src/Header.jsx
+++ src/Header.jsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 export function Header() {
     return (
         <header>
-            <h2>Reddit for our own</h2>
+            <h2>Reddit for our own </h2>
         </header>
     );
 } 
